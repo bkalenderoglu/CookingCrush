7d70d6f8d4c64aafbcf53822989a3b3fab677fa9
bkalenderoglu <bkalenderoglu1@gmail.com>
2023-09-17 17:24:23 +0300

    adding container / pick-drop functionality

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,7 +8,26 @@ public class ClearCounter : BaseCounter {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player) {
-        
+        if (!HasKitchenObject()) {
+            // no kitchen object
+            if (player.HasKitchenObject()) {
+                // player grabbed object
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else {
+                // player has nothing to carry
+            }
+        }
+        else {
+            // there is a kitchen object
+            if (player.HasKitchenObject()) {
+                // player carrying something
+            }
+            else {
+                // player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 
     
