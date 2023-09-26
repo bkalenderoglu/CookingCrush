ef09f99f4c75932706d1c9d85fa0147e672b6734
bkalenderoglu <bkalenderoglu1@gmail.com>
2023-09-26 14:31:33 +0300

    recipe icons are added

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ClearCounter : BaseCounter {
-    
-    
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player) {
@@ -22,6 +20,21 @@ public class ClearCounter : BaseCounter {
             // there is a kitchen object
             if (player.HasKitchenObject()) {
                 // player carrying something
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+                    // player is holding a plate
+                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO())) {
+                        GetKitchenObject().DestroySelf();
+                    }
+                }
+                else {
+                    // player is not carrying plate but something else
+                    if (GetKitchenObject().TryGetPlate(out plateKitchenObject)) {
+                        // counter has a plate
+                        if (plateKitchenObject.TryAddIngredient(player.GetKitchenObject().GetKitchenObjectSO())) {
+                            player.GetKitchenObject().DestroySelf();
+                        }
+                    }
+                }
             }
             else {
                 // player is not carrying anything
@@ -29,6 +42,4 @@ public class ClearCounter : BaseCounter {
             }
         }
     }
-
-    
 }
--- Assets/Scripts/Counters/CuttingCounter.cs
+++ Assets/Scripts/Counters/CuttingCounter.cs
@@ -41,6 +41,12 @@ public class CuttingCounter : BaseCounter, IHasProgress {
             // there is a kitchen object
             if (player.HasKitchenObject()) {
                 // player carrying something
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+                    // player is holding a plate
+                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO())) {
+                        GetKitchenObject().DestroySelf();
+                    }
+                }
             }
             else {
                 // player is not carrying anything
