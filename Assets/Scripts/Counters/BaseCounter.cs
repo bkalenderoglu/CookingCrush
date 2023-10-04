b533bece72016147415d023ac87edbdce2841a1c
bkalenderoglu <bkalenderoglu1@gmail.com>
2023-10-04 20:50:49 +0300

    menu / clearing statics

--- Assets/Scripts/Counters/BaseCounter.cs
+++ Assets/Scripts/Counters/BaseCounter.cs
@@ -4,8 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseCounter : MonoBehaviour, IKitchenObjectParent {
+
     public static event EventHandler OnAnyObjectPlacedHere;
-    
+
+    public static void ResetStaticData() {
+        OnAnyObjectPlacedHere = null;
+    }
+
     [SerializeField] private Transform counterTopPoint;
 
     private KitchenObject kitchenObject;
--- Assets/Scripts/Counters/TrashCounter.cs
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -4,8 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TrashCounter : BaseCounter {
+
     public static event EventHandler OnAnyObjectTrashed;
-    
+
+    new public static void ResetStaticData() {
+        OnAnyObjectTrashed = null;
+    }
+
     public override void Interact(Player player) {
         if (player.HasKitchenObject()) {
             player.GetKitchenObject().DestroySelf();
